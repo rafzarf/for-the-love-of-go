2031abcac3c5c168e44b4d02a312301fc3236a5c
rafzarf <xxrafzaxx@gmail.com>
2023-09-07 16:31:37 +0700

    first case: calculator

---
+++ calculator/calculator.go
@@ -0,0 +1,33 @@
+package calculator
+
+import (
+	"errors"
+	"math"
+	"fmt"
+)
+
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func Divide(a, b float64) (float64, error) {
+	if b == 0{
+		return 0, errors.New("division by zero not allowed")
+	}
+	return a / b, nil
+}
+
+func Sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, fmt.Errorf("square root of negative number not allowed: %f", a)
+	}
+	return math.Sqrt(a), nil
+}
